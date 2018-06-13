cf233fa2db62157e62c76680dc4f4d21239df49d
HEXER22 <40181915+HEXER22@users.noreply.github.com>
2018-06-13 22:37:53 +0530

    Add files via upload

---
+++ Recondog.sh
@@ -0,0 +1,21 @@
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+
+
+pkg install Python2
+
+
+apt-get install git -y
+
+
+git clone https://github.com/UltimateHackers/ReconDog.git
+
+
+
+cd  ReconDog
+
+
+
+python2 dog.py
+
+
+echo "Thanks For using HEXER22 script"
